312483163ff6eba26e55f4423eeb872ed1c5d13a
Alex R <areniak147@hotmail.com>
2024-09-12 17:17:10 -0400

    added a thank you page

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,28 +2,36 @@ let scrollpos = window.scrollY;
 const navbar = document.querySelector('.navbar-list');
 
 const addShadowOnScroll = () => navbar.classList.add('navbar-list-shadow');
-const removeShadowOnScroll = () => navbar.classList.remove('navbar-list-shadow');
+const removeShadowOnScroll = () =>
+  navbar.classList.remove('navbar-list-shadow');
 
 window.addEventListener('scroll', () => {
-    scrollpos = window.scrollY;
+  scrollpos = window.scrollY;
 
-    if(scrollpos >= 95) {
-        addShadowOnScroll();
-    } else {
-        removeShadowOnScroll();
-    }
+  if (scrollpos >= 95) {
+    addShadowOnScroll();
+  } else {
+    removeShadowOnScroll();
+  }
 });
 
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if(entry.isIntersecting) {
-            entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show')
-        }
-    });
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
 });
 
 const hiddenElements = document.querySelectorAll('.hidden');
 
-hiddenElements.forEach(el => observer.observe(el));
+hiddenElements.forEach((el) => observer.observe(el));
+
+// Clear form after submission
+window.onbeforeunload = () => {
+  for (const form of document.getElementsByTagName('form')) {
+    form.reset();
+  }
+};
